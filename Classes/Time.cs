a7af039557bd81600c4c2c1d38091462e77f3431
Piotr Tarczynski <paszczakx@gmail.com>
2020-05-13 11:39:46 +0200

    Time converter implementation

---
+++ Classes/Time.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BerlinClock.Classes
+{
+    public class Time
+    {
+        public int Hours { get; private set; }
+        public int Minutes { get; private set; }
+        public int Seconds { get; private set; }
+
+        public static Time Parse(string time)
+        {
+            if (string.IsNullOrWhiteSpace(time))
+                throw new ArgumentException("Time cannot be empty", nameof(time));
+
+            var split = time.Split(':');
+            if (split.Length != 3)
+                throw new ArgumentException("Invalid time format", nameof(time));
+
+            return new Time()
+            {
+                Hours = ParseTimePart(split[0], 24),
+                Minutes = ParseTimePart(split[1], 59),
+                Seconds = ParseTimePart(split[2], 59)
+            };
+        }
+
+        private static int ParseTimePart(string input, int maxValue)
+        {
+            if (int.TryParse(input, out int value) && value >= 0 && value <= maxValue)
+                return value;
+            else
+                throw new ArgumentException("Invalid time format");
+        }
+    }
+}
--- Classes/TimeConverter.cs
+++ Classes/TimeConverter.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BerlinClock.Classes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,7 +10,35 @@ namespace BerlinClock
     {
         public string convertTime(string aTime)
         {
-            throw new NotImplementedException();
+            return string.Join(Environment.NewLine, 
+                Clock.Zip(GetLampsInRows(Time.Parse(aTime)), (row, lamps) => FormatLampString(row, lamps)));
+        }
+
+        private static string FormatLampString(string lampPattern, int lampsOn)
+        {
+            return lampPattern.Substring(0, lampsOn) + new string(LampOff, lampPattern.Length - lampsOn);
+        }
+
+        private static IEnumerable<int> GetLampsInRows(Time time)
+        {
+            yield return 1 - time.Seconds % 2;
+            yield return time.Hours / 5;
+            yield return time.Hours % 5;
+            yield return time.Minutes / 5;
+            yield return time.Minutes % 5;
+        }
+
+        private const char LampOff = 'O';
+        private static IEnumerable<string> Clock
+        {
+            get
+            {
+                yield return "Y";
+                yield return "RRRR";
+                yield return "RRRR";
+                yield return "YYRYYRYYRYY";
+                yield return "YYYY";
+            }
         }
     }
 }
